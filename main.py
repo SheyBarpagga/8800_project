d199cae37d8939085fcddd8af2b4d7b6fdddd837
SheyBarpagga <sheybarpagga@gmail.com>
2024-11-06 04:51:00 -0800

    test

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+import sys
+# Before any more imports, leave cwd out of sys.path for internal 'conda shell.*' commands.
+# see https://github.com/conda/conda/issues/6549
+if len(sys.argv) > 1 and sys.argv[1].startswith('shell.') and sys.path and sys.path[0] == '':
+    # The standard first entry in sys.path is an empty string,
+    # and os.path.abspath('') expands to os.getcwd().
+    del sys.path[0]
+
 import os
 import numpy as np
 import librosa
@@ -100,14 +109,12 @@ def predict(audio_path, model_path, vocab):
     return prediction
 
 
-def main(args):
+def main():
+
+    audio_path = "./phishing/27488_normalized.wav"
+
     model_path = "multi_input_model.pth"
     vocab = torch.load("vocab.pth")
-    predict(args, model_path, vocab)
+    predict(audio_path, model_path, vocab)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <path to testing audio>")
-        sys.exit(1)
-    main(sys.argv[1])
+main()
