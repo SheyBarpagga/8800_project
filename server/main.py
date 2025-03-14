1a3094b3a6445335a832b26de4e26ba900283c96
SheyBarpagga <sheybarpagga@gmail.com>
2025-03-14 12:40:00 -0700

    milestone 4

--- server/main.py
+++ server/main.py
@@ -4,10 +4,11 @@ import librosa
 import numpy as np
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Body
 from fastapi.responses import HTMLResponse
-from pydub import AudioSegment
+# from pydub import AudioSegment
 from PIL import Image
 from torchvision import transforms
 from new_model import MultiInputModel
+from new_model import cnn_with_lstm
 from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 import matplotlib.pyplot as plt
 import sys
@@ -16,10 +17,17 @@ from nltk.tokenize import word_tokenize
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 from typing import List
 import json
+import base64
+from io import BytesIO
+import asyncio
+import time
+import soundfile as sf
+# import ffmpeg
 
 nltk.download('punkt')
 
@@ -33,7 +41,7 @@ def audio_to_spectrogram(audio_path, save_path):
     plt.axis("off")
     plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
     plt.close()
-    print(f"Spectrogram saved at: {save_path}")
+    # print(f"Spectrogram saved at: {save_path}")
 
 # Function to generate MFCC
 def extract_mfcc(audio_path, save_path):
@@ -44,15 +52,9 @@ def extract_mfcc(audio_path, save_path):
     plt.axis("off")
     plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
     plt.close()
-    print(f"MFCC saved at: {save_path}")
+    # print(f"MFCC saved at: {save_path}")
     return mfccs
 
-# # Function to generate transcript
-# def transcribe_audio(audio_path):
-#     result = transcription_model.transcribe(audio_path)
-#     transcript = result['text']
-#     print(f"Transcript: {transcript}")
-#     return transcript
 def transcribe_audio(audio_path):
     # Load audio
     y, sr = librosa.load(audio_path, sr=16000)
@@ -81,7 +83,7 @@ def predict(audio_path, model, vocab):
     transcript = transcribe_audio(audio_path)
     
     # Preprocessing
-    print("Preprocessing data...")
+    # print("Preprocessing data...")
     spectrogram = Image.open(spectrogram_path).convert('RGB')
     mfcc = Image.open(mfcc_path).convert('RGB')
     spectrogram = transforms.ToTensor()(spectrogram).unsqueeze(0)
@@ -99,27 +101,36 @@ def predict(audio_path, model, vocab):
     numerical_tokens = torch.tensor(numerical_tokens).unsqueeze(0)
     
     # Make a prediction
-    print("Making prediction...")
+    # print("Making prediction...")
     with torch.no_grad():
         output = model(spectrogram, mfcc, numerical_tokens)
         prediction = (output.squeeze() > 0.5).float().item()
-    
+    # app.mount("/static", StaticFiles(directory="."), name="static")
     print(f"Prediction: {prediction}")
     return prediction
+
 
 if __name__ == "__main__":
 
     app = FastAPI()
 
     # Load the models
-    vocab = torch.load("vocab_2.pth")
+    vocab = torch.load("vocab_2.pth", weights_only=False)
     vocab_size = len(vocab)
     model = MultiInputModel(vocab_size=vocab_size)
     model.load_state_dict(torch.load("multi_input_model_2.pth"))
     model.eval()
 
-    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
-    transcription_model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+    path = os.path.join(os.path.dirname(__file__), 'cnn_lstm_model.pth')
+
+    image_model = cnn_with_lstm()
+    image_model.load_state_dict(torch.load(path, weights_only=False, map_location=torch.device('cpu')))
+    image_model.eval()
+
+    # processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+    # transcription_model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
+    transcription_model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h-lv60-self")
 
     os.makedirs('audio_chunks', exist_ok=True)
 
@@ -134,13 +145,42 @@ if __name__ == "__main__":
 
     @app.get("/")
     async def get():
-        with open('C:\\Users\\sheyb\\Documents\\8800_project\\server\\templates\\index.html', 'r') as file:
+        # with open('C:\\Users\\sheyb\\Documents\\8800_project\\server\\templates\\index.html', 'r') as file:
+        path = os.path.join(os.path.dirname(__file__), 'templates\\index.html')
+        with open(path, 'r') as file:            
             return HTMLResponse(file.read())
         
     @app.get("/call")
     async def get():
-        with open('C:\\Users\\sheyb\\Documents\\8800_project\\server\\templates\\client.html', 'r') as file:
+        # with open('C:\\Users\\sheyb\\Documents\\8800_project\\server\\templates\\client.html', 'r') as file:
+        path = os.path.join(os.path.dirname(__file__), 'templates\\client.html')
+        with open(path, 'r') as file:
             return HTMLResponse(file.read())
+    
+    @app.get("/mfcc")
+    async def get_mfcc():
+        path = os.path.join(os.path.dirname(__file__), 'temp_mfcc.png')
+        return FileResponse(path)
+
+    @app.get("/spectrogram")
+    async def get_spectrogram():
+        path = os.path.join(os.path.dirname(__file__), 'temp_spectrogram.png')
+        return FileResponse(path)
+    
+    @app.get("/test1")
+    async def get_spectrogram():
+        test_path = os.path.join(os.path.dirname(__file__), 'test1.png')
+        return FileResponse(test_path)
+    
+    @app.get("/test2")
+    async def get_spectrogram():
+        test_path = os.path.join(os.path.dirname(__file__), 'test2.png')
+        return FileResponse(test_path)
+
+    @app.get("/test3")
+    async def get_spectrogram():
+        test_path = os.path.join(os.path.dirname(__file__), 'test3.png')
+        return FileResponse(test_path)
 
     connected_clients = set()
 
@@ -171,25 +211,171 @@ if __name__ == "__main__":
         os.remove(audio_path)  # Clean up the temporary file
         return {"scam": prediction}
 
+
+    audio_queue = asyncio.Queue()
+
+    async def process_audio_queue():
+        while True:
+            await asyncio.sleep(5)
+            audio_data = await audio_queue.get()
+            
+            # Save temp file
+            audio_path = "audio_chunks/temp_audio.wav"
+            with open(audio_path, "wb") as f:
+                f.write(audio_data)
+
+            # Predict
+            prediction = predict(audio_path, model, vocab)
+            
+            # Send response
+            await active_websocket.send_json({"scam": prediction})
+            
+
+
     @app.websocket("/audio-stream")
-    async def websocket_endpoint(websocket: WebSocket):
+    async def audio_stream(websocket: WebSocket):
+        global active_websocket
+        active_websocket = websocket
         await websocket.accept()
-        audio_data = bytearray()
+        
+        asyncio.create_task(process_audio_queue())  # Start processing
+        
         try:
             while True:
                 data = await websocket.receive_bytes()
-                audio_data.extend(data)
-                # Process every 5 seconds of audio
-                if len(audio_data) > 16000 * 5: 
-                    audio_path = "audio_chunks/temp_audio.wav"
-                    with open(audio_path, "wb") as f:
-                        f.write(audio_data)
-                    prediction = predict(audio_path, model, vocab)
-                    await websocket.send_json({"scam": prediction})
-                    # Reset the buffer
-                    audio_data = bytearray()  
+                await audio_queue.put(data)
+        
         except WebSocketDisconnect:
-            print("WebSocket disconnected")
+            print("Audio WebSocket disconnected")
+
+    # @app.websocket("/video-stream")
+    # async def video_stream(websocket: WebSocket):
+    #     await websocket.accept()
+        
+    #     frame_sequence = []  
+        
+    #     try:
+    #         while True:
+    #             data = await websocket.receive_text()
+    #             frames_data = json.loads(data)["frames"]
+                
+    #             for frame in frames_data:
+    #                 # Extract `data` from the dictionary structure
+    #                 base64_data = frame['data']  
+
+    #                 image_bytes = base64.b64decode(base64_data)
+    #                 image = Image.open(BytesIO(image_bytes)).convert("RGB")
+                    
+    #                 # Preprocess image
+    #                 transform = transforms.Compose([
+    #                     transforms.Resize((128, 128)),
+    #                     transforms.ToTensor(),
+    #                     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    #                 ])
+    #                 image_tensor = transform(image)
+    #                 frame_sequence.append(image_tensor)
+
+    #             if len(frame_sequence) == 10:
+    #                 sequence_tensor = torch.stack(frame_sequence).unsqueeze(0)  
+    #                 frame_sequence.clear()  # Clear for the next set
+                    
+    #                 # Predict
+    #                 with torch.no_grad():
+    #                     output = image_model(sequence_tensor)
+    #                     prediction = (output.squeeze() > 0.5).float().item()
+    #                     print(f"Video Prediction: {prediction}")
+                    
+    #                 await websocket.send_json({"scam": prediction})
+        
+    #     except WebSocketDisconnect:
+    #         print("Video WebSocket disconnected")
+
+
+    def load_images_from_folder(folder_path):
+        image_paths = sorted([os.path.join(folder_path, img) for img in os.listdir(folder_path) if img.endswith(('png', 'jpg', 'jpeg'))])
+        return image_paths[:10] 
+
+    @app.post("/test-images1")
+    async def test_images1():
+        test1_path = os.path.join(os.path.dirname(__file__), 'test1')
+        return test_images(test1_path)
+
+    @app.post("/test-images2")
+    async def test_images2():
+        test_path = os.path.join(os.path.dirname(__file__), 'test2')
+        return test_images(test_path)
+    
+    @app.post("/test-images3")
+    async def test_images3():
+        test_path = os.path.join(os.path.dirname(__file__), 'test3')
+        return test_images(test_path)
+    
+    def test_images(test):
+        # folder_path = "test"
+        image_paths = load_images_from_folder(test)
+
+        frame_sequence = []
+        transform = transforms.Compose([
+            transforms.Resize((128, 128)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        ])
+
+        for image_path in image_paths:
+            image = Image.open(image_path).convert("RGB")
+            image_tensor = transform(image)
+            frame_sequence.append(image_tensor)
+
+        sequence_tensor = torch.stack(frame_sequence).unsqueeze(0)
+
+        # Predict
+        with torch.no_grad():
+            output = image_model(sequence_tensor)
+            prediction = (output.squeeze() > 0.5).float().item()
+
+        return JSONResponse({"scam": prediction})
+
+
+    @app.websocket("/video-stream")
+    async def video_stream(websocket: WebSocket):
+        await websocket.accept()
+        
+        frame_sequence = []  
+        
+        try:
+            while True:
+                data = await websocket.receive_text()
+                frames_data = json.loads(data)["frames"]
+                
+                for frame_data in frames_data:
+                    image_bytes = base64.b64decode(frame_data)
+                    image = Image.open(BytesIO(image_bytes)).convert("RGB")
+                    
+                    # Preprocess image
+                    transform = transforms.Compose([
+                        transforms.Resize((128, 128)),
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+                    ])
+                    image_tensor = transform(image)
+                    frame_sequence.append(image_tensor)
+
+                    
+                if len(frame_sequence) == 10:
+                    sequence_tensor = torch.stack(frame_sequence).unsqueeze(0)  
+                    # Clear for the next set
+                    frame_sequence.clear()  
+                    
+                    # Predict
+                    with torch.no_grad():
+                        output = image_model(sequence_tensor)
+                        prediction = (output.squeeze() > 0.5).float().item()
+                        print(f"Video Prediction: {prediction}")
+                    
+                    await websocket.send_json({"scam": prediction})
+        
+        except WebSocketDisconnect:
+            print("Video WebSocket disconnected")
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
--- server/new_model.py
+++ server/new_model.py
@@ -78,6 +78,54 @@ class MultiInputModel(nn.Module):
         return output
 
 
+class cnn_with_lstm(nn.Module):
+    def __init__(self, dropout_rate=0.5, lstm_hidden_size=64):
+        super(cnn_with_lstm, self).__init__()
+        
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),  
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),  
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),  
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+
+        self.global_avg_pool = nn.AdaptiveAvgPool2d(1) 
+        # LSTM Layer
+        self.lstm = nn.LSTM(input_size=128, hidden_size=lstm_hidden_size, batch_first=True)
+        
+        self.fc1 = nn.Linear(lstm_hidden_size, 128)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc_out = nn.Linear(64, 1)
+
+    def forward(self, x):
+        batch_size, seq_len, c, h, w = x.shape 
+        x = x.view(batch_size * seq_len, c, h, w) 
+        
+        x = self.cnn(x) 
+        x = self.global_avg_pool(x)  
+        x = x.view(batch_size, seq_len, -1)
+    
+        x, _ = self.lstm(x)
+        x = x[:, -1, :]  # take output from last time step
+        
+        x = torch.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = torch.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc_out(x)
+        
+        return x
 # import torch
 # from torchviz import make_dot
 # import os
